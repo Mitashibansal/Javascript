c4998832e0607311f9b1dceeeae28f949cc2eeb1
Mitashibansal <84284893+Mitashibansal@users.noreply.github.com>
2022-06-22 21:44:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+console.time()
+console.log('hello console')
+console.log([34,3,1,2])
+console.table({mitashi:'this',marks:34})
+console.warn('this is a warning')
+console.timeEnd()
+var age =56;
+console.assert(age>189,'older than 189 age is not possible')
+console.error('This is an error.')
+console.clear()
+var name =`Mit"ashi`
+console.log(age,name)
+{
+    let age= 89// creates a local variable in the code 
+    age=13
+    console.log(age)
+}
+console.log(age)
+const arr1=[23,45,32,98,85]
+console.log(arr1.sort())
+console.log(arr1.push(78))
+arr1.push(90)
+console.log(arr1)
+//by using const we can emit changes in array but we cant reinitializes he value.
+/* Most common programing case types:
+1. camelCase
+2. kebab-case
+3. snake_case
+4. PascalCase
+*/
+var myString = 'JavaScript!!!'
+console.log(myString.charAt(7)) 
